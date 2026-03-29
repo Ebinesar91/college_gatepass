b1891db1092175bf9d287ab97f262e11cfb0105a
Ebinesar91 <ebinesarebinesar192@gmail.com>
2026-03-29 23:58:33 +0530

    fix: integrate /migrate/ tool into root system urls for 100% reachability

--- college_permission_system/urls.py
+++ college_permission_system/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from permissions.views import run_migrations
+
 urlpatterns = [
+    path('migrate/', run_migrations, name='system_migrate'),
     path('admin/', admin.site.urls),
     path('', include('permissions.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
